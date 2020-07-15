346838beeb137ccc69676f6ebb8bdac38727cc90
testandobot <68176907+testandobot@users.noreply.github.com>
2020-07-15 14:47:42 -0300

    atual

--- santos.py
+++ santos.py
@@ -1,23 +1,28 @@
+import os
 import tweepy
 import time
+import datetime
+from os import environ
 
-auth = tweepy.OAuthHandler('3aRyfr1fD2ybyyRcgb7mFg3R5', 'maed6uLK7e8BdZHTGZsR5iiSDuaESsuRZLzE4g8jGp2ukYycKv')
-auth.set_access_token('2186612092-2vFsuN7Q5YLxzUJP5XMIavPrYryseDexCPVGkLt', 'Yo7GqeEuacHiNXuB8mTWM3eLVrTvfUnF0QOlxLlSimCMv')
+CONSUMER_KEY = os.environ.get('CONSUMER_KEY')
+CONSUMER_SECRET = os.environ.get('CONSUMER_SECRET')
+ACCESS_KEY = os.environ.get('ACCESS_KEY')
+ACCESS_SECRET = os.environ.get('ACCESS_SECRET')
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
-user = api.me()
-print(user)
 
 class MyStreamListener(tweepy.StreamListener):
     def on_status(self, status):
                 if status.user.screen_name == "SantosFC" and not "RT" in status.text:
-                    print(status.text)
-                    try:                     
+                    try:
+                     print(status.text)
                      api.create_favorite(status.id)
-                     # api.retweet(status.id)
+                     api.retweet(status.id)
                     except tweepy.TweepError as e:
                      print(e.reason)
-nmTuitar = 0
-myStreamListener = MyStreamListener()
-myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener) 
 
+myStreamListener = MyStreamListener()
+myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
 myStream.filter(follow=["49751816"])
